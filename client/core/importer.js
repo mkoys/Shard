5002a6efb32600735b51b0837ac8994d23e6c0cf
mkoys <mkoys@protonmail.com>
2022-03-17 10:00:30 +0100

    Changed to custom html class based components

--- client/core/importer.js
+++ client/core/importer.js
@@ -1,93 +1,58 @@
-export default class Importer {
+let component, folder;
+
+class customComponenet extends HTMLElement {
     constructor() {
-        this.modules = [];
+
+        super();
         this.states = [];
-    }
 
-    import(module) {
-        this.modules.push(module);
-        this.render();
-    }
+        this.attachShadow({ mode: 'open' });
 
-    state(name, value) {
-        this.states.push({
-            key: name,
-            value: value,
-            element: []
-        })
+        let moduleStyle = document.createElement("style");
+        let moduleBody = document.createElement("div");
 
-        const index = this.states.findIndex(item => item.key === name);
+        moduleStyle.innerHTML = component.moduleStyle.innerHTML;
+        moduleBody.setAttribute("class", component.moduleBody.getAttribute("class"))
+        moduleBody.innerHTML = component.moduleBody.innerHTML;
 
-        return (value) => {
-            this.states[index].value = value;
-            this.states[index].element.forEach(elm => {
-                elm.innerHTML = value;
-            });
-        }
-    }
+        this.shadowRoot.append(moduleStyle, moduleBody);
 
-    async render() {
-        this.modules.forEach(async module => {
-            const result = await fetch(`./template/${module}.html`);
-
-            const textResult = await result.text();
-
-            const domParser = new DOMParser();
-
-            const moduleDocument = domParser.parseFromString(textResult, "text/html");
-
-            const moduleStyle = moduleDocument.querySelector("style");
-            const moduleBody = moduleDocument.body.children[0];
-            const moduleScript = moduleDocument.querySelector("script");
+        this.definedText = this.getAttribute("text");
 
 
-            if (moduleStyle) {
-                document.head.appendChild(moduleStyle);
-            }
+        this.definedText.split(",").forEach(item => {
+            const values = item.split(":");
 
-            const allTemplates = document.querySelectorAll(`shard-${module}`);
+            const textElement = this.shadowRoot.querySelector(`text#${values[0]}`);
 
-            allTemplates.forEach(element => {
-                const allSetAtr = moduleBody.querySelectorAll("[type]");
-
-                allSetAtr.forEach(atr => {
-                    const val = atr.getAttribute("set");
-
-                    const realVal = val.split(":");
-
-                    atr.setAttribute(realVal[0], element.getAttribute(realVal[1]));
-                });
-
-                element.root = element;
-                element.outerHTML = moduleBody.outerHTML;
-
-                const allText = element.getAttribute("text");
-                const group = allText.split(",");
-                group.forEach(textGroup => {
-                    const text = textGroup.split(":");
-                    const currentText = document.querySelector(`text#${text[0]}`);
-                    const parent = currentText.parentElement;
-                    if (text[1].startsWith("{") && text[1].endsWith("}")) {
-                        let value = text[1];
-                        value = value.replace("{", "");
-                        value = value.replace("}", "");
-                        const stateIndex = this.states.findIndex(item => item.key === value);
-
-                        this.states[stateIndex].element.push(parent);
-                        currentText.outerHTML = this.states[stateIndex].value;
-                    } else {
-                        currentText.outerHTML = text[1];
-                    }
-
-                });
-            });
-
-            if (moduleScript) {
-                const newScript = document.createElement("script");
-                newScript.setAttribute("type", "module");
-                newScript.innerHTML = moduleScript.innerHTML;
-                document.head.appendChild(newScript);
-            }
+            textElement.outerHTML = values[1];
         });
+
+
+        this.shadowRoot.querySelector("slot").outerHTML = this.innerHTML;
     }
 }
+
+function setFolder(newfolder) {
+    folder = newfolder;
+}
+
+async function add(module) {
+    const result = await fetch(`./${folder}/${module}.html`);
+
+    const textResult = await result.text();
+
+    const domParser = new DOMParser();
+
+    const moduleDocument = domParser.parseFromString(textResult, "text/html");
+
+    const moduleStyle = moduleDocument.querySelector("style");
+    const moduleBody = moduleDocument.body.children[0];
+    const moduleScript = moduleDocument.querySelector("script");
+
+    component = { moduleScript, moduleBody, moduleStyle };
+
+    customElements.define(`shard-${module}`, customComponenet);
+}
+
+export default { add, setFolder };
